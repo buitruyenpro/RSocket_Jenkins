11349f8a55dd506dded754b7016a62f6a035caaa
DESKTOP-GOOSPHF\admin <namtruyenpro95@gmail.com>
2023-04-09 11:34:27 +0700

    Add print test

--- src/main/java/com/demo/rs_socket/RsSocketApplication.java
+++ src/main/java/com/demo/rs_socket/RsSocketApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RsSocketApplication {
 
 	public static void main(String[] args) {
+		System.out.println("=========================================RsSocketApplication====================================");
 		SpringApplication.run(RsSocketApplication.class, args);
 	}
 
