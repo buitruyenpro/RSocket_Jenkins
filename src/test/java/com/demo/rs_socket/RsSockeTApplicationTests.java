c2b1293826ec63b01652c041bde3fc6e37126bff
DESKTOP-GOOSPHF\admin <namtruyenpro95@gmail.com>
2023-04-09 12:44:13 +0700

    Update unitest

--- src/test/java/com/demo/rs_socket/RsSockeTApplicationTests.java
+++ src/test/java/com/demo/rs_socket/RsSockeTApplicationTests.java
@@ -1,5 +1,5 @@
 package com.demo.rs_socket;
-
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,6 +9,24 @@ class RsSockeTApplicationTests {
 	@Test
 	void contextLoads() {
 		System.out.println("run......................");
+	}
+	@Test
+	void testAddition() {
+		int a = 5;
+		int b = 10;
+		assertEquals(15, a + b);
+	}
+
+	@Test
+	void testStringLength() {
+		String str = "Hello, World!";
+		assertEquals(13, str.length());
+	}
+
+	@Test
+	void testArraySize() {
+		int[] arr = {1, 2, 3, 4, 5};
+		assertEquals(5, arr.length);
 	}
 
 }
